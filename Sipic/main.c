2a8a8d1788d65063518acc48798677500644341a
Alexis Ouellet-Patenaude <aop007@godzimac-2.local>
2012-10-02 23:15:03 -0400

    Initial Commit

---
+++ Sipic/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sipic
+//
+//  Created by Alexis Ouellet-Patenaude on 2012-10-02.
+//  Copyright (c) 2012 Alexis Ouellet-Patenaude. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
