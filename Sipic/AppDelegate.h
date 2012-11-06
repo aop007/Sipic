956a1097ccf8e588b6a8fedba5424d10d26a2980
Alexis Ouellet Patenaude <alexis.ouellet.patenaude@gmail.com>
2012-11-05 22:02:46 -0500

    Added UI files to project.

---
+++ Sipic/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  SipicUI
+//
+//  Created by Alexis Ouellet-Patenaude on 2012-11-05.
+//  Copyright (c) 2012 Alexis Ouellet-Patenaude. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "main.h"
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
