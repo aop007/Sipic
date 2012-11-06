440b1ccde328cafda893ff17af19192ab7d1d9f4
Alexis Ouellet Patenaude <alexis.ouellet.patenaude@gmail.com>
2012-11-05 21:19:40 -0500

    Added Xcode UI files.

---
+++ SipicUI/SipicUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SipicUI
+//
+//  Created by Alexis Ouellet-Patenaude on 2012-11-05.
+//  Copyright (c) 2012 Alexis Ouellet-Patenaude. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SipicUI/SipicUI/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  SipicUI
+//
+//  Created by Alexis Ouellet-Patenaude on 2012-11-05.
+//  Copyright (c) 2012 Alexis Ouellet-Patenaude. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
